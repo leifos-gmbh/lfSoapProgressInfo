6d543d1d6db49d88520414cfe0bab4db25acae9e
Marvin Barz <barz@leifos.de>
2020-07-21 11:47:56 +0200

    Edited plugin.php to allow functioning of Plugin on 5.4.9 version of ILIAS

--- plugin.php
+++ plugin.php
@@ -8,7 +8,7 @@ $version = "0.0.1";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.4.13";
+$ilias_min_version = "5.4.9";
 $ilias_max_version = "5.4.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
